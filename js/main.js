06e915260a7c4d50dc40fe3b40e7b3a68b2a5f0c
DEJENERUTA <45858936+DEJENERUTA@users.noreply.github.com>
2021-08-26 22:37:08 +0200

    added js-bootcamp-1 solution

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const siteFooter = document.querySelector(".siteFooter__quote");
+const citatArray = [
+	"Den der kommer først til mølle, får først malet",
+	"Man skal ikke kaste med sten når man selv bor i glashus",
+	"Én fugl i hånden er bedre end ti på taget",
+	"Tomme tønder buldrer mest",
+	"Man skal ikke gå over åen efter vand",
+	"Brændt barn skyr ilden",
+	"Når katten er ude, spiller musene på bordet",
+	"Liden tue kan vælte stort læs",
+	"Lille og vågen er bedre end stor og doven",
+	"Mange bække små gør en stor å",
+];
+//console.log(citatArray);
+
+function citat() {
+	let findCitat = citatArray[Math.floor(Math.random() * citatArray.length)];
+	// console.log(findCitat);
+	siteFooter.textContent = citatArray[findCitat];
+}
+citat();
